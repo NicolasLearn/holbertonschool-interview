9aacf628af39ad65ab7a171787fbcaab84e0d20c
nicolas <nicolas.quoirez@hotmail.fr>
2024-09-27 12:05:38 +0200

    fix betty style error

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,41 +1,46 @@
 #include "sandpiles.h"
 
-
 /**
- * 
+ * is_stable - Checks if a sandpile is stable
+ * @grid: 3x3 grid to check
+ *
+ * Return: 1 if stable, 0 if not stable
  */
 int is_stable(int grid[3][3])
 {
-  int i, j;
+	int i, j;
 
-  for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-    {
-      if (grid[i][j] > 3)
-        return (0);
-    }
-  }
-    return (1);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+				return (0);
+		}
+	}
+	return (1);
 }
 
-
 /**
- * 
+ * grid_toppler - Stabilizes a 3x3 sandpile by toppling cells
+ * @grid: 3x3 grid to stabilize
+ *
+ * Description: Topple cells in a sandpile that have more than 3 grains.
+ * Distribute the excess grains to the neighboring cells.
  */
 void grid_toppler(int grid[3][3])
 {
-  int i, j;
-  int temp_grid[3][3] = {0};
+	int i, j;
+	int temp_grid[3][3] = {0};
 
-  for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-    {
-      if (grid[i][j] > 3)
-      {
-        temp_grid[i][j] -= 4;
-        if (i + 1 < 3)
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				temp_grid[i][j] -= 4;
+				if (i + 1 < 3)
 					temp_grid[i + 1][j]++;
 				if (i - 1 >= 0)
 					temp_grid[i - 1][j]++;
@@ -43,21 +48,22 @@ void grid_toppler(int grid[3][3])
 					temp_grid[i][j + 1]++;
 				if (j - 1 >= 0)
 					temp_grid[i][j - 1]++;
-      }
-    }
-  }
-  for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-      grid[i][j] += temp_grid[i][j];
-  }
+			}
+		}
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			grid[i][j] += temp_grid[i][j];
+	}
 }
 
-
 /**
- * no_static_print_grid - Print 3x3 grid
- * @grid: 3x3 grid
+ * no_static_print_grid - Print a 3x3 grid
+ * @grid: 3x3 grid to print
  *
+ * Description: Prints the grid with space-separated values, row by row.
  */
 void no_static_print_grid(int grid[3][3])
 {
@@ -76,24 +82,27 @@ void no_static_print_grid(int grid[3][3])
 	}
 }
 
-
 /**
- * 
+ * sandpiles_sum - Sum of two 3x3 sandpiles and stabilizes the result
+ * @grid1: First 3x3 grid (result will be stored here)
+ * @grid2: Second 3x3 grid to add to the first
+ *
+ * Description: Adds the two grids and stabilizes the result using toppling
+ * operations until the grid is stable. Prints the grid after each toppling.
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-  int i, j;
+	int i, j;
 
-  for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-      grid1[i][j] += grid2[i][j];
-  }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			grid1[i][j] += grid2[i][j];
+	}
 
-  while (!is_stable(grid1))
-  {
-    no_static_print_grid(grid1);
-    grid_toppler(grid1);
-  }
-  
+	while (!is_stable(grid1))
+	{
+		no_static_print_grid(grid1);
+		grid_toppler(grid1);
+	}
 }
