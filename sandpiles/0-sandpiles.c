43ecbcd16939ac7fa66e82cd08d1097a9ef4103a
nicolas <nicolas.quoirez@hotmail.fr>
2024-09-27 11:03:27 +0200

    first trying checkeur

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,15 +1,93 @@
 #include "sandpiles.h"
 
+
+/**
+ * 
+ */
+int is_stable(int grid[3][3])
+{
+  int i, j;
+
+  for (i = 0; i < 3; i++)
+  {
+    for (j = 0; j < 3; j++)
+    {
+      if (grid[i][j] > 3)
+        return (0);
+    }
+  }
+    return (1);
+}
+
+
+/**
+ * 
+ */
+void grid_toppler(int grid[3][3])
+{
+  int i, j;
+
+  for (i = 0; i < 3; i++)
+  {
+    for (j = 0; j < 3; j++)
+    {
+      if (grid[i][j] > 3)
+      {
+        grid[i][j] -= 4;
+        if (i + 1 < 3)
+					grid[i + 1][j]++;
+				if (i - 1 >= 0)
+					grid[i - 1][j]++;
+				if (j + 1 < 3)
+					grid[i][j + 1]++;
+				if (j - 1 >= 0)
+					grid[i][j - 1]++;
+      }
+    }
+  }
+}
+
+
+/**
+ * no_static_print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
+ */
+void no_static_print_grid(int grid[3][3])
+{
+	int i, j;
+
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
 /**
  * 
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+  int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-		for (j = 0; j < 3; j++)
-			grid1[i][j] += grid2[i][j];
-    }
+  for (i = 0; i < 3; i++)
+  {
+    for (j = 0; j < 3; j++)
+      grid1[i][j] += grid2[i][j];
+  }
+
+  while (!is_stable(grid1))
+  {
+    no_static_print_grid(grid1);
+    grid_toppler(grid1);
+  }
+  
 }
--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -5,5 +5,8 @@
 #include <stdlib.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int is_stable(int grid[3][3]);
+void grid_toppler(int grid[3][3]);
+void no_static_print_grid(int grid[3][3]);
 
 #endif
